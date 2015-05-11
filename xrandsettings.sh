fbef5c19fa5fe5f0ed493048946d0947adf725a9
Riyad Twair <riyad.twair@lazada.com>
2015-05-11 13:19:36 +0700

    added extra keycode remappings

--- xrandsettings.sh
+++ xrandsettings.sh
@@ -2,3 +2,7 @@
 xrandr --output DP1 --mode "2560x1080"
 #xrandr --output DP1 --left-of eDP1
 xmodmap -e "keycode 169= Insert"
+xmodmap -e "keycode 169= Insert"
+xmodmap -e "keycode 66= F3"
+xmodmap -e "clear lock"
+xscreensaver -no-splash &
